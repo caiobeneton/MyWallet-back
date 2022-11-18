708301524aa6318e2406fe6f9ee89ad53ed555d8
Caio Beneton <caio.beneton@gmail.com>
2022-11-17 22:11:56 -0300

    feat: get user balance

--- src/controllers/balance.controller.js
+++ src/controllers/balance.controller.js
@@ -1,7 +1,22 @@
-import { balanceCollection } from "../database/db";
+import { balanceCollection, sessionsCollection, usersCollection } from "../database/db";
 
 export async function getBalance(req, res) {
+    const { authorization } = req.headers;
 
+    const token = authorization?.replace("Bearer ", "");
+    if (!token) {
+        return res.sendStatus(401);
+    }
+
+    try {
+        const sessions = await sessionsCollection.findOne({ token })
+        const user = await usersCollection.findOne({ _id: sessions.userId })
+        const balance = await balanceCollection.find({user}).toArray()
+
+        res.send(balance)
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 export async function inputBalance(req, res) {
