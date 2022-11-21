e1252873d2a9ed3011d37a2cdb9f87f5cea63958
Caio Beneton <caio.beneton@gmail.com>
2022-11-21 13:44:31 -0300

    feat: input balance

--- src/controllers/balance.controller.js
+++ src/controllers/balance.controller.js
@@ -20,5 +20,28 @@ export async function getBalance(req, res) {
 }
 
 export async function inputBalance(req, res) {
+    const { description, value, type } = req.body
+    const {authorization} = req.headers
 
+    const token = authorization?.replace("Bearer ", "");
+    if (!token) {
+        return res.sendStatus(401);
+    }
+
+    try {
+        const sessions = await sessionsCollection.findOne({ token })
+        const user = await usersCollection.findOne({ _id: sessions.userId })
+
+        const newInput = {
+            description,
+            value,
+            type,
+            user: user._id
+        }
+
+        await balanceCollection.insertOne({newInput})
+        res.sendStatus(201)
+    } catch (error) {
+        res.sendStatus(500);
+    }
 }
